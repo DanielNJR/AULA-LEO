7d1711cd18ff78784ab9d38ded5bedd6eb8c94dc
DanielNJR <abreudaniel189@gmail.com>
2026-09-16 13:22:50 -0300

    Implement usuarioController function
    Fix syntax error in foreach loop and improve output.

---
+++ controllers/usuario.controller.php
@@ -0,0 +1,8 @@
+<?php
+function usuarioController(){
+  echo "6. Controller recebeu a requisição.<br>";
+  $usuarios = usuarioService();
+  echo "8. Controller recebeu .<br>";
+  echo "Usuarios encontrados:<br>;
+  foreach {$usuarios as $usuario) {
+    echo "- " .$usuarios . "<br>";
